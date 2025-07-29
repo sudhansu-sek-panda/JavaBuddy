ac2641d058effbe71ce2bc35aee9e354e1aa0736
soumyaranjan123-arch <ranjansoumya33479@gmail.com>
2025-07-29 21:20:12 +0530

    fix navbar-color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -43,15 +43,12 @@ const Navbar = () => {
   return (
     <div className="w-full">
       <nav
-        className={`h-[60px] px-6 md:px-10 flex justify-between items-center fixed top-0 left-0 right-0 z-50 transition-transform duration-700 ${
-          showNavbar ? "translate-y-0" : "-translate-y-full"
-        } shadow`}
-        style={{
-          background: "linear-gradient(90deg, #ff6b6b, #fca311, #1982c4)",
-        }}
+        className={`h-[60px] px-6 md:px-10 flex justify-between items-center fixed top-0 left-0 right-0 z-50 transition-transform duration-700
+    ${showNavbar ? "translate-y-0" : "-translate-y-full"}
+    bg-sky-800/30 text-orange-400 backdrop-blur-md shadow-md border-b border-orange-200/20`}
       >
         {/* Logo */}
-        <div className="text-3xl font-extrabold tracking-wide flex items-center gap-2 text-white">
+        <div className="text-3xl font-extrabold tracking-wide flex items-center gap-2 ">
           {/* <GraduationCap className="w-7 h-7 text-white" /> */}
           <NavLink
             to="/"
@@ -62,7 +59,7 @@ const Navbar = () => {
         </div>
 
         {/* Desktop Navigation */}
-        <ul className="hidden md:flex space-x-8 font-semibold text-white items-center">
+        <ul className="hidden md:flex space-x-8 font-semibold items-center">
           <li className="flex items-center gap-1 hover:scale-110 hover:text-yellow-300 transition duration-300">
             <BookOpenText className="w-5 h-5" />
             <NavLink to="/about">About</NavLink>
@@ -121,7 +118,7 @@ const Navbar = () => {
 
       {/* Mobile menu */}
       {mobileMenuOpen && (
-        <div className="md:hidden bg-white px-6 py-4 space-y-4 shadow-md">
+        <div className="md:hidden bg-sky-700 text-orange-700 px-6 py-4 space-y-4 shadow-md">
           <NavLink
             to="/about"
             className="block text-gray-700 font-semibold"
@@ -165,4 +162,4 @@ const Navbar = () => {
   );
 };
 
-  export default Navbar;
+export default Navbar;
